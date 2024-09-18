b4e8cc5d93e42901aa44d8ca242ee9d0b8cd61d1
dosepehr <dosepehr@gmail.com>
2024-09-18 20:17:58 +0330

    generate random coordinates for every object

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('object')
+const objects = [
+  {
+    object: 'r',
+    count: 20
+  },
+  {
+    object: 'p',
+    count: 20
+  },
+  {
+    object: 's',
+    count: 20
+  },
+]
+const sizes = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
+
+objects.forEach(object => {
+  for (let i = 0; i < object.count; i++) {
+    const randomPosition = { x: Math.random() * sizes.w, y: Math.random() * sizes.h }
+    console.log(object.object, randomPosition)
+  }
+})
