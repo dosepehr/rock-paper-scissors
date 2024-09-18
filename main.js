36b49c47a94691c0c73199fe0f7f741c35828ba9
dosepehr <dosepehr@gmail.com>
2024-09-18 22:21:29 +0330

    bring objects in a small square in middle

--- main.js
+++ main.js
@@ -14,14 +14,24 @@ const objects = [
     count: 20
   },
 ]
+
+const squareSize = 400;
 const sizes = {
   w: window.innerWidth,
   h: window.innerHeight,
 };
+
+const squareStartX = (sizes.w / 2) - (squareSize / 2);
+const squareStartY = (sizes.h / 2) - (squareSize / 2);
+
 let objectsData = [];
+
 objects.forEach(object => {
   for (let i = 0; i < object.count; i++) {
-    const randomCoords = { x: Math.random() * sizes.w, y: Math.random() * sizes.h }
+    const randomCoords = { 
+      x: squareStartX + Math.random() * squareSize,  
+      y: squareStartY + Math.random() * squareSize  
+    }
     objectsData.push({ content: object.object, ...randomCoords })
   }
 })
@@ -41,10 +51,13 @@ const ObjectsGenerator = (objectsData) => {
 ObjectsGenerator(objectsData)
 
 const changeCoords = setInterval(() => {
-  container.innerHTML = '';
+  container.innerHTML = '';  
+  
   const newCoords = objectsData.map((obj) => {
-    return { content: obj.content, x: obj.x + ((Math.random() - 0.5) * 10), y: obj.y + ((Math.random() - 0.5) * 10) }
+    const newX = Math.max(squareStartX, Math.min(squareStartX + squareSize, obj.x + ((Math.random() - 0.5) * 10)));
+    const newY = Math.max(squareStartY, Math.min(squareStartY + squareSize, obj.y + ((Math.random() - 0.5) * 10)));
+    return { content: obj.content, x: newX, y: newY };
   });
 
-  ObjectsGenerator(newCoords)
+  ObjectsGenerator(newCoords);
 }, 100);
